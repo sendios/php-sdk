c8a70e40cd7d60eb1396beb2358cb4ce78673b3f
Voloshyn <volodymyr.voloshyn@corp.mailfire.io>
2021-04-30 10:54:41 +0300

    SLS-708

--- src/SendiosSdk.php
+++ src/SendiosSdk.php
@@ -8,7 +8,6 @@ use Sendios\Resources\Buying;
 use Sendios\Resources\ClientUser;
 use Sendios\Resources\Email;
 use Sendios\Resources\Event;
-use Sendios\Resources\Goal;
 use Sendios\Resources\Push;
 use Sendios\Resources\Unsub;
 use Sendios\Resources\UnsubTypes;
@@ -29,7 +28,6 @@ use Sendios\Services\ErrorHandler;
  * @property Unsub $unsub
  * @property UnsubTypes $unsubTypes
  * @property Webpush $webpush
- * @property Goal $goal
  * @property Event $event
  * @property ClientUser $clientUser
  * @property Encrypter $encrypter
