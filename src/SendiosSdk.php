28a27e7e9a58798a4801f3ae43476d0a7209f207
Maksym Dorontsev <maxim.doroncev@megogo.net>
2022-11-29 15:39:07 +0200

    ASAP-781 Fix error handler forwarding into request object

--- src/SendiosSdk.php
+++ src/SendiosSdk.php
@@ -32,18 +32,27 @@ class SendiosSdk
      * @var int
      */
     public $clientId;
-
     /**
      * @var string
      */
     public $clientKey;
-
     /**
      * @var Encrypter
      */
     public $encrypter;
-
-    private const RESOURCES_PROPERTIES = ['email', 'user', 'unsub', 'unsubTypes', 'webpush', 'content', 'event', 'clientUser', 'buying', 'push', 'request'];
+    private const RESOURCES_PROPERTIES = [
+        'email',
+        'user',
+        'unsub',
+        'unsubTypes',
+        'webpush',
+        'content',
+        'event',
+        'clientUser',
+        'buying',
+        'push',
+        'request',
+    ];
     /**
      * @var ErrorHandler
      */
@@ -79,31 +88,33 @@ class SendiosSdk
      */
     public function __get(string $name)
     {
+        if (property_exists($this, $name)) {
+            return $this->{$name};
+        }
+
         if (!in_array($name, self::RESOURCES_PROPERTIES, true)) {
             throw new WrongResourceRequestedException("Requested property {$name} is not in resources list");
         }
-        if (!property_exists($this, $name)) {
-            switch ($name) {
-                case 'push' : {
-                    $this->push = new Push($this->clientId, $this->encrypter, $this->errorHandler, $this->request);
-                    return $this->push;
-                }
-                case 'unsub' : {
-                    $this->unsub = new Unsub($this->user, $this->errorHandler, $this->request);
-                    return $this->unsub;
-                }
-                case 'request' : {
-                    return new Request($this->clientId, $this->clientKey, $this->errorHandler);
-                }
-                default : {
-                    $className = 'Sendios\Resources\\' . ucfirst($name);
-                    $this->{$name} = new $className($this->errorHandler, $this->request);
-                    return $this->{$name};
-                }
-            }
-        }
+        switch ($name) {
+            case 'push' :
+                $this->push = new Push($this->clientId, $this->encrypter, $this->errorHandler, $this->request);
 
-        return $this->{$name};
+                return $this->push;
+
+            case 'unsub' :
+                $this->unsub = new Unsub($this->user, $this->errorHandler, $this->request);
+
+                return $this->unsub;
+            case 'request' :
+                $this->request = new Request($this->clientId, $this->clientKey, $this->errorHandler);
+
+                return $this->request;
+            default :
+                $className = 'Sendios\Resources\\' . ucfirst($name);
+                $this->{$name} = new $className($this->errorHandler, $this->request);
+
+                return $this->{$name};
+        }
     }
 
     public function setErrorHandler(ErrorHandler $errorHandler): void
